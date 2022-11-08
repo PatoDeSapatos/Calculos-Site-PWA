defcc96b98f11d08ac9bc10d998d9f16fe5fc9b7
Pato de Sapato <112494130+PatoDeSapatos@users.noreply.github.com>
2022-11-08 11:36:35 -0300

    html, css and js created

---
+++ trabalho_calculo/js/index.js
@@ -0,0 +1,16 @@
+function sumir(){
+    /*
+    var novoP = document.createElement("p")
+    var novoTxt = document.createTextNode("joão lixo")
+    var tamanho = 0;
+    tamanho++;
+
+    novoP.appendChild(novoTxt)
+    document.body.appendChild(novoP)
+
+    novoP.clientWidth(tamanho)
+    */
+
+    var divPrincipal = document.getElementById("placa")
+    divPrincipal.style.display = "none";
+}
